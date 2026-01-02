f2ca32097f643cf9ecf355da284cf1eb9243b33c
Rishitha <kotyadarishitha25@gmail.com>
2026-01-02 15:05:07 +0530

    Initial backend setup

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("ABCD")
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
